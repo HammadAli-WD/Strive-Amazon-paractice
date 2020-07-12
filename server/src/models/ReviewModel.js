59ef32404c973362523b521d9dd1ffaa23ad928d
Hammad-87 <pakswim@gmail.com>
2020-07-12 21:31:45 +0200

    Review

---
+++ server/src/models/ReviewModel.js
@@ -0,0 +1,31 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const ReviewSchema = new Schema(
+ {
+    comment: {
+      type: String,
+      required: true,
+    },
+    rate: {
+      type: Number,
+      required: true,
+      max: 5,
+      min: 0,
+    },
+   /*  productId: {
+      type: String,
+      required: true,
+    }, */
+  },
+  {
+    timestamps: true,
+  }
+)
+
+/* ReviewSchema.static("getReviewsByProductID", async function (id){
+    const reviews = await ReviewModel.find(id)
+    return reviews
+}) */
+const ReviewModel = mongoose.model("Review", ReviewSchema);
+module.exports = ReviewModel;
